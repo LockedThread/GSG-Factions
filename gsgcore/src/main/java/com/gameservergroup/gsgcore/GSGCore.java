b9212544685e20159d5f10a314af283559ed7c12
Trent Williams <proteinshakedev@gmail.com>
2019-02-27 21:04:59 -0500

    Removed EventConsumer shit, it's a no go for appearance and code readability

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/GSGCore.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/GSGCore.java
@@ -1,9 +1,7 @@
 package com.gameservergroup.gsgcore;
 
-import com.gameservergroup.gsgcore.events.EventConsumerImpl;
 import com.gameservergroup.gsgcore.events.EventPost;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GSGCore extends JavaPlugin {
@@ -13,14 +11,6 @@ public class GSGCore extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        EventConsumerImpl<PlayerJoinEvent> s = new EventConsumerImpl<PlayerJoinEvent>() {
-            @Override
-            public void accept(PlayerJoinEvent playerJoinEvent) {
-
-            }
-        };
-
 
         EventPost.of(AsyncPlayerChatEvent.class)
                 .filter(event -> event.getMessage().contains("a"))
