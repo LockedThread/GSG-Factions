51daf714f0aec57964c9ecc1321568e73993ddcc
Trent Williams <proteinshakedev@gmail.com>
2019-03-03 13:02:10 -0500

    Impl of CommandMapUtil

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/commands/CommandBuilder.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/commands/CommandBuilder.java
@@ -1,7 +1,6 @@
 package com.gameservergroup.gsgcore.commands;
 
-import com.gameservergroup.gsgcore.GSGCore;
-import com.gameservergroup.gsgcore.commands.handler.CommandHandler;
+import com.gameservergroup.gsgcore.commands.handler.FunctionalCommandHandler;
 import com.gameservergroup.gsgcore.commands.handler.ICommandHandler;
 import com.gameservergroup.gsgcore.commands.post.CommandPost;
 import com.gameservergroup.gsgcore.utils.CommandMapUtil;
@@ -11,22 +10,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashSet;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class CommandBuilder<T extends CommandSender> {
 
-    private static final Predicate<ICommandHandler<? extends CommandSender>> IS_PLAYER = commandHandler -> commandHandler.getSender() instanceof Player;
-    private static final Predicate<ICommandHandler<? extends CommandSender>> IS_CONSOLE = commandHandler -> commandHandler.getSender() instanceof ConsoleCommandSender;
-
-    private Consumer<CommandHandler<T>> consumer;
     private HashSet<Predicate<ICommandHandler<? extends CommandSender>>> predicates;
     private CommandPost commandPost;
     private FunctionalCommandHandler<T> functionalCommandHandler;
 
-    public CommandBuilder(HashSet<Predicate<ICommandHandler<? extends CommandSender>>> predicates, CommandPost commandPost) {
+    private CommandBuilder(HashSet<Predicate<ICommandHandler<? extends CommandSender>>> predicates, CommandPost commandPost, FunctionalCommandHandler<T> functionalCommandHandler) {
         this.predicates = predicates;
         this.commandPost = commandPost;
+        this.functionalCommandHandler = functionalCommandHandler;
     }
 
     public CommandBuilder(CommandPost commandPost) {
@@ -34,14 +29,32 @@ public class CommandBuilder<T extends CommandSender> {
         this.predicates = new HashSet<>();
     }
 
+    @SuppressWarnings("unchecked")
     public CommandBuilder<Player> assertPlayer() {
-        predicates.add(IS_PLAYER);
-        return new CommandBuilder<>(predicates, commandPost);
+        predicates.add(commandHandler -> {
+            if (commandHandler.getSender() instanceof Player) {
+                return true;
+            }
+            commandHandler.reply("&cYou may only use this command as a Player!");
+            return false;
+        });
+        final CommandBuilder<Player> playerCommandBuilder = new CommandBuilder<>(predicates, commandPost, (FunctionalCommandHandler<Player>) functionalCommandHandler);
+        commandPost.setCommandBuilder(playerCommandBuilder);
+        return playerCommandBuilder;
     }
 
+    @SuppressWarnings("unchecked")
     public CommandBuilder<ConsoleCommandSender> assertConsole() {
-        predicates.add(IS_CONSOLE);
-        return new CommandBuilder<>(predicates, commandPost);
+        predicates.add(commandHandler -> {
+            if (commandHandler.getSender() instanceof ConsoleCommandSender) {
+                return true;
+            }
+            commandHandler.reply("&cYou may only use this command as Console!");
+            return false;
+        });
+        final CommandBuilder<ConsoleCommandSender> playerCommandBuilder = new CommandBuilder<>(predicates, commandPost, (FunctionalCommandHandler<ConsoleCommandSender>) functionalCommandHandler);
+        commandPost.setCommandBuilder(playerCommandBuilder);
+        return playerCommandBuilder;
     }
 
     public CommandBuilder<T> filter(Predicate<ICommandHandler<? extends CommandSender>> predicate) {
@@ -49,14 +62,9 @@ public class CommandBuilder<T extends CommandSender> {
         return this;
     }
 
-    public CommandBuilder<T> handle(Consumer<CommandHandler<T>> consumer) {
-        this.consumer = consumer;
-        return this;
-    }
-
     public void post(Plugin plugin, String... aliases) {
         CommandPost.getCommandPosts().put(aliases, commandPost);
-        CommandMapUtil.registerCommand(plugin, GSGCore.getInstance().getCommandPostExecutor(), aliases);
+        CommandMapUtil.registerCommand(plugin, aliases);
     }
 
     public HashSet<Predicate<ICommandHandler<? extends CommandSender>>> getPredicates() {
