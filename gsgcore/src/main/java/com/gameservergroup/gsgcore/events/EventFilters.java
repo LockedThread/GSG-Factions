4de30806c2fec5ccd87eaa480b384ed2e00d0186
Trent Williams <proteinshakedev@gmail.com>
2019-03-05 16:38:01 -0500

    Created getters for EventFilters

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
@@ -1,6 +1,5 @@
 package com.gameservergroup.gsgcore.events;
 
-
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -9,8 +8,19 @@ import java.util.function.Predicate;
 
 public class EventFilters {
 
-    public static final Predicate<PlayerMoveEvent> IGNORE_SAME_CHUNK = event -> event.getFrom().getChunk().getX() == event.getTo().getChunk().getX() && event.getFrom().getChunk().getZ() == event.getTo().getChunk().getZ();
-    public static final Predicate<? extends Cancellable> IGNORE_CANCELLED = cancellable -> !cancellable.isCancelled();
-    public static final Predicate<? extends PlayerEvent> IGNORE_HAND_NULL = event -> event.getPlayer().getItemInHand() != null;
+    private static final Predicate<PlayerMoveEvent> IGNORE_SAME_CHUNK = event -> event.getFrom().getChunk().getX() == event.getTo().getChunk().getX() && event.getFrom().getChunk().getZ() == event.getTo().getChunk().getZ();
+    private static final Predicate<? extends Cancellable> IGNORE_CANCELLED = cancellable -> !cancellable.isCancelled();
+    private static final Predicate<? extends PlayerEvent> IGNORE_HAND_NULL = event -> event.getPlayer().getItemInHand() != null;
 
+    public static Predicate<PlayerMoveEvent> getIgnoreSameChunk() {
+        return IGNORE_SAME_CHUNK;
+    }
+
+    public static Predicate<? extends Cancellable> getIgnoreCancelled() {
+        return IGNORE_CANCELLED;
+    }
+
+    public static Predicate<? extends PlayerEvent> getIgnoreHandNull() {
+        return IGNORE_HAND_NULL;
+    }
 }
