5e7b807368d965de9fa8bea7ce78221f8a637a7e
Trent Williams <proteinshakedev@gmail.com>
2019-03-06 21:43:00 -0500

    Added more EventFilters and formatted them properly

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
@@ -1,26 +1,35 @@
 package com.gameservergroup.gsgcore.events;
 
+import com.gameservergroup.gsgcore.menus.Menu;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.function.Predicate;
 
+@SuppressWarnings("unchecked")
 public class EventFilters {
 
     private static final Predicate<PlayerMoveEvent> IGNORE_SAME_CHUNK = event -> event.getFrom().getChunk().getX() == event.getTo().getChunk().getX() && event.getFrom().getChunk().getZ() == event.getTo().getChunk().getZ();
     private static final Predicate<? extends Cancellable> IGNORE_CANCELLED = cancellable -> !cancellable.isCancelled();
     private static final Predicate<? extends PlayerEvent> IGNORE_HAND_NULL = event -> event.getPlayer().getItemInHand() != null;
+    private static final Predicate<? extends InventoryEvent> IGNORE_NON_MENUS = (Predicate<InventoryEvent>) event -> event instanceof InventoryClickEvent ? ((InventoryClickEvent) event).getClickedInventory() instanceof Menu : event.getInventory().getHolder() instanceof Menu;
+
+    public static <T extends InventoryEvent> Predicate<T> getIgnoreNonMenus() {
+        return (Predicate<T>) IGNORE_NON_MENUS;
+    }
+
+    public static <T extends Cancellable> Predicate<T> getIgnoreCancelled() {
+        return (Predicate<T>) IGNORE_CANCELLED;
+    }
 
     public static Predicate<PlayerMoveEvent> getIgnoreSameChunk() {
         return IGNORE_SAME_CHUNK;
     }
 
-    public static Predicate<? extends Cancellable> getIgnoreCancelled() {
-        return IGNORE_CANCELLED;
-    }
-
-    public static Predicate<? extends PlayerEvent> getIgnoreHandNull() {
-        return IGNORE_HAND_NULL;
+    public static <T extends PlayerEvent> Predicate<T> getIgnoreHandNull() {
+        return (Predicate<T>) IGNORE_HAND_NULL;
     }
 }
