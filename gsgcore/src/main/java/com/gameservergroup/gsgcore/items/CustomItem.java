c9cd8fbb9408aabdf9b22fbae2a94e26254a212a
Trent Williams <proteinshakedev@gmail.com>
2019-03-09 01:05:49 -0500

    Changed from section constructor in CustomItem to ItemStackBuilder

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/items/CustomItem.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/items/CustomItem.java
@@ -1,10 +1,7 @@
 package com.gameservergroup.gsgcore.items;
 
 import com.gameservergroup.gsgcore.utils.NBTItem;
-import org.bukkit.DyeColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
@@ -25,43 +22,7 @@ public class CustomItem {
     }
 
     public static CustomItem of(ConfigurationSection section, String name) {
-        final ItemStackBuilder itemStackBuilder = ItemStackBuilder.of(Material.matchMaterial(section.getString("material")));
-        if (section.isSet("amount")) {
-            if (section.isInt("amount")) {
-                itemStackBuilder.setAmount(section.getInt("amount"));
-            } else {
-                throw new RuntimeException(section.getCurrentPath() + ".amount can't be parsed as an integer");
-            }
-        }
-        if (section.isSet("data")) {
-            if (section.isString("data")) {
-                final DyeColor dyeColor = parseDyeColor(section.getString("data"));
-                if (dyeColor != null) {
-                    itemStackBuilder.setDyeColor(dyeColor);
-                } else {
-                    itemStackBuilder.setData(Short.parseShort(section.getString("data")));
-                }
-            } else {
-                itemStackBuilder.setData((short) section.getInt("data"));
-            }
-        }
-        if (section.isSet("color")) {
-            if (section.isColor("color")) {
-                itemStackBuilder.setColor(section.getColor("color"));
-            } else {
-                throw new RuntimeException(section.getCurrentPath() + ".color can't be parsed as a org.bukkit.Color");
-            }
-        }
-        if (section.isSet("name") && section.isString("name")) {
-            itemStackBuilder.setDisplayName(section.getString("name"));
-        }
-        if (section.isSet("lore") && section.isList("lore")) {
-            itemStackBuilder.setLore(section.getStringList("lore"));
-        }
-        if (section.isSet("itemflags") && section.isList("itemflags")) {
-            itemStackBuilder.addItemFlags(section.getStringList("itemflags").stream().map(CustomItem::parseItemFlag).toArray(ItemFlag[]::new));
-        }
-        return of(itemStackBuilder, name);
+        return of(ItemStackBuilder.of(section), name);
     }
 
     public static CustomItem of(ConfigurationSection configurationSection) {
@@ -70,22 +31,6 @@ public class CustomItem {
 
     public static CustomItem of(ItemStackBuilder itemStackBuilder, String name) {
         return new CustomItem(name, new NBTItem(itemStackBuilder.build()).set(name, true).buildItemStack());
-    }
-
-    private static DyeColor parseDyeColor(String s) {
-        try {
-            return DyeColor.valueOf(s.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Unable to parse " + s + " as a Bukkit DyeColor", e);
-        }
-    }
-
-    private static ItemFlag parseItemFlag(String s) {
-        try {
-            return ItemFlag.valueOf(s.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Unable to parse " + s + " as a Bukkit DyeColor", e);
-        }
     }
 
     public static HashMap<String, CustomItem> getCustomItems() {
--- gsgcore/src/main/java/com/gameservergroup/gsgcore/items/ItemStackBuilder.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/items/ItemStackBuilder.java
@@ -4,6 +4,7 @@ import com.gameservergroup.gsgcore.utils.Text;
 import org.bukkit.Color;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -27,6 +28,46 @@ public class ItemStackBuilder {
 
     private ItemStackBuilder(Material material, DyeColor dyeColor) {
         this.itemStack = new ItemStack(material, material == Material.WOOL ? dyeColor.getWoolData() : dyeColor.getDyeData());
+    }
+
+    public static ItemStackBuilder of(ConfigurationSection section) {
+        final ItemStackBuilder itemStackBuilder = ItemStackBuilder.of(Material.matchMaterial(section.getString("material")));
+        if (section.isSet("amount")) {
+            if (section.isInt("amount")) {
+                itemStackBuilder.setAmount(section.getInt("amount"));
+            } else {
+                throw new RuntimeException(section.getCurrentPath() + ".amount can't be parsed as an integer");
+            }
+        }
+        if (section.isSet("data")) {
+            if (section.isString("data")) {
+                final DyeColor dyeColor = parseDyeColor(section.getString("data"));
+                if (dyeColor != null) {
+                    itemStackBuilder.setDyeColor(dyeColor);
+                } else {
+                    itemStackBuilder.setData(Short.parseShort(section.getString("data")));
+                }
+            } else {
+                itemStackBuilder.setData((short) section.getInt("data"));
+            }
+        }
+        if (section.isSet("color")) {
+            if (section.isColor("color")) {
+                itemStackBuilder.setColor(section.getColor("color"));
+            } else {
+                throw new RuntimeException(section.getCurrentPath() + ".color can't be parsed as a org.bukkit.Color");
+            }
+        }
+        if (section.isSet("name") && section.isString("name")) {
+            itemStackBuilder.setDisplayName(section.getString("name"));
+        }
+        if (section.isSet("lore") && section.isList("lore")) {
+            itemStackBuilder.setLore(section.getStringList("lore"));
+        }
+        if (section.isSet("itemflags") && section.isList("itemflags")) {
+            itemStackBuilder.addItemFlags(section.getStringList("itemflags").stream().map(ItemStackBuilder::parseItemFlag).toArray(ItemFlag[]::new));
+        }
+        return itemStackBuilder;
     }
 
     public static ItemStackBuilder of(Material material) {
@@ -86,5 +127,19 @@ public class ItemStackBuilder {
         return itemStack;
     }
 
+    private static DyeColor parseDyeColor(String s) {
+        try {
+            return DyeColor.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Unable to parse " + s + " as a Bukkit DyeColor", e);
+        }
+    }
 
+    private static ItemFlag parseItemFlag(String s) {
+        try {
+            return ItemFlag.valueOf(s.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Unable to parse " + s + " as a Bukkit DyeColor", e);
+        }
+    }
 }
