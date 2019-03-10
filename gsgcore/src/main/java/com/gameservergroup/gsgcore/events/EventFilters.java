8d9adf0f3db27385075c5869fd2c5e8e22f20430
Trent Williams <proteinshakedev@gmail.com>
2019-03-09 22:34:10 -0500

    Added more applications for IGNORE_HAND_NULL (PlayerEvent, BlockBreakEvent, BlockPlaceEvent)

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/events/EventFilters.java
@@ -2,6 +2,9 @@ package com.gameservergroup.gsgcore.events;
 
 import com.gameservergroup.gsgcore.menus.Menu;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.player.PlayerEvent;
@@ -14,8 +17,17 @@ public class EventFilters {
 
     private static final Predicate<PlayerMoveEvent> IGNORE_SAME_CHUNK = event -> event.getFrom().getChunk().getX() == event.getTo().getChunk().getX() && event.getFrom().getChunk().getZ() == event.getTo().getChunk().getZ();
     private static final Predicate<? extends Cancellable> IGNORE_CANCELLED = cancellable -> !cancellable.isCancelled();
-    private static final Predicate<? extends PlayerEvent> IGNORE_HAND_NULL = event -> event.getPlayer().getItemInHand() != null;
     private static final Predicate<? extends InventoryEvent> IGNORE_NON_MENUS = (Predicate<InventoryEvent>) event -> event instanceof InventoryClickEvent ? ((InventoryClickEvent) event).getClickedInventory().getHolder() instanceof Menu : event.getInventory().getHolder() instanceof Menu;
+    private static final Predicate<? extends Event> IGNORE_HAND_NULL = event -> {
+        if (event instanceof PlayerEvent) {
+            return ((PlayerEvent) event).getPlayer().getItemInHand() != null;
+        } else if (event instanceof BlockBreakEvent) {
+            return ((BlockBreakEvent) event).getPlayer().getItemInHand() != null;
+        } else if (event instanceof BlockPlaceEvent) {
+            return ((BlockPlaceEvent) event).getItemInHand() != null;
+        }
+        return false;
+    };
 
     public static <T extends InventoryEvent> Predicate<T> getIgnoreNonMenus() {
         return (Predicate<T>) IGNORE_NON_MENUS;
@@ -29,7 +41,7 @@ public class EventFilters {
         return IGNORE_SAME_CHUNK;
     }
 
-    public static <T extends PlayerEvent> Predicate<T> getIgnoreHandNull() {
+    public static <T extends Event> Predicate<T> getIgnoreHandNull() {
         return (Predicate<T>) IGNORE_HAND_NULL;
     }
 }
