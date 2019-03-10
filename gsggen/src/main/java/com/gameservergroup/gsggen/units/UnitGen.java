5bb75ebf9fcb85eb729ad6a07e2e604d635a1516
Trent Williams <proteinshakedev@gmail.com>
2019-03-10 14:59:06 -0400

    Added disableLavaBucketPlayerUse, check for material, fixed specific data checks from their name to data, and edited default config.

--- gsggen/src/main/java/com/gameservergroup/gsggen/units/UnitGen.java
+++ gsggen/src/main/java/com/gameservergroup/gsggen/units/UnitGen.java
@@ -3,12 +3,16 @@ package com.gameservergroup.gsggen.units;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.gameservergroup.gsgcore.commands.post.CommandPost;
 import com.gameservergroup.gsgcore.enums.Direction;
+import com.gameservergroup.gsgcore.events.EventPost;
 import com.gameservergroup.gsgcore.storage.JsonFile;
 import com.gameservergroup.gsgcore.units.Unit;
 import com.gameservergroup.gsggen.GSGGen;
 import com.gameservergroup.gsggen.generation.Generation;
 import com.gameservergroup.gsggen.objs.Gen;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,7 +22,7 @@ public class UnitGen extends Unit {
 
     private static final GSGGen GSG_GEN = GSGGen.getInstance();
 
-    private boolean closeInventoryOnPurchase, closeInventoryOnNoMoney;
+    private boolean closeInventoryOnPurchase, closeInventoryOnNoMoney, disableLavaBucketPlayerUse;
     private Set<Generation> generations;
     private HashMap<String, Gen> genHashMap = new HashMap<>();
 
@@ -28,7 +32,7 @@ public class UnitGen extends Unit {
         this.generations = jsonFile.load().orElse(new HashSet<>());
         this.closeInventoryOnNoMoney = GSG_GEN.getConfig().getBoolean("menu.options.close-inventory-on-no-money");
         this.closeInventoryOnPurchase = GSG_GEN.getConfig().getBoolean("menu.options.close-inventory-on-purchase");
-
+        this.disableLavaBucketPlayerUse = GSG_GEN.getConfig().getBoolean("disable-lava-bucket-player-use");
         hookDisable(() -> jsonFile.save(generations));
 
         for (String genKey : GSG_GEN
@@ -39,21 +43,24 @@ public class UnitGen extends Unit {
             double price = 0.0;
             Direction direction = null;
             boolean patch = false;
+            Material material = null;
             ConfigurationSection section = GSG_GEN.getConfig().getConfigurationSection("gens." + genKey);
             for (String data : section.getKeys(false)) {
                 if (data.equalsIgnoreCase("price")) {
-                    price = section.getDouble("price");
+                    price = section.getDouble(data);
                 } else if (data.equalsIgnoreCase("direction")) {
-                    direction = Direction.valueOf(section.getString("direction").toUpperCase());
+                    direction = Direction.valueOf(section.getString(data).toUpperCase());
                 } else if (data.equalsIgnoreCase("patch")) {
-                    patch = section.getBoolean("patch");
+                    patch = section.getBoolean(data);
                 } else if (data.equalsIgnoreCase("length")) {
-                    length = section.getInt("length");
+                    length = section.getInt(data);
+                } else if (data.equalsIgnoreCase("material")) {
+                    material = Material.matchMaterial(section.getString(data));
                 } else if (!data.equalsIgnoreCase("menu") && !data.equalsIgnoreCase("item")) {
                     throw new RuntimeException("We've found an un-parsable key in " + section.getCurrentPath() + " called " + data);
                 }
             }
-            genHashMap.put(genKey, length <= 0 ? new Gen(section, direction, price, patch) : new Gen(section, direction, price, patch, length));
+            genHashMap.put(genKey, length <= 0 ? new Gen(section, direction, price, patch, material) : new Gen(section, direction, price, patch, length, material));
         }
 
         CommandPost.of()
@@ -61,6 +68,16 @@ public class UnitGen extends Unit {
                 .assertPlayer()
                 .handler(c -> c.getSender().openInventory(GSG_GEN.getGenMenu().getInventory()))
                 .post(GSG_GEN, "gen", "gens", "genblock", "genblocks", "gb", "genbucket", "genbuckets");
+
+        EventPost.of(PlayerBucketEmptyEvent.class)
+                .filter(event -> disableLavaBucketPlayerUse)
+                .filter(event -> event.getItemStack() != null && event.getItemStack().getType() == Material.LAVA_BUCKET)
+                .handle(event -> event.setCancelled(true));
+
+        EventPost.of(PlayerBucketFillEvent.class)
+                .filter(event -> disableLavaBucketPlayerUse)
+                .filter(event -> event.getItemStack() != null && event.getItemStack().getType() == Material.LAVA_BUCKET)
+                .handle(event -> event.setCancelled(true));
     }
 
     public Set<Generation> getGenerations() {
@@ -77,5 +94,9 @@ public class UnitGen extends Unit {
 
     public boolean isCloseInventoryOnNoMoney() {
         return closeInventoryOnNoMoney;
+    }
+
+    public boolean isDisableLavaBucketPlayerUse() {
+        return disableLavaBucketPlayerUse;
     }
 }
