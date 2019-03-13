fb3b02caebc611aca5e69bd4223fa6962ad82149
Trent Williams <proteinshakedev@gmail.com>
2019-03-13 16:12:04 -0400

    Redid the method for stability purposes with odd number'd worldborders

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/utils/Utils.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/utils/Utils.java
@@ -1,12 +1,15 @@
 package com.gameservergroup.gsgcore.utils;
 
 import org.bukkit.Location;
+import org.bukkit.WorldBorder;
 
 public class Utils {
 
-    public static boolean isInWorldBorder(Location location) {
-        double length = location.getWorld().getWorldBorder().getSize() / 2;
-        return Math.abs(location.getWorld().getWorldBorder().getCenter().getX() - location.getX()) < length &&
-                Math.abs(location.getWorld().getWorldBorder().getCenter().getZ() - location.getZ()) < length;
+    public static boolean isOutsideBorder(Location location) {
+        WorldBorder worldBorder = location.getWorld().getWorldBorder();
+        double size = worldBorder.getSize() / 2.0;
+        double centerX = worldBorder.getCenter().getX();
+        double centerZ = worldBorder.getCenter().getZ();
+        return centerX - size > location.getX() || centerX + size <= location.getX() || centerZ - size > location.getZ() || centerZ + size <= location.getZ();
     }
 }
