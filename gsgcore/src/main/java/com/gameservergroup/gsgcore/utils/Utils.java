f62d7d3bdf83522a82b3e8b415ad6de139d032ce
Trent Williams <proteinshakedev@gmail.com>
2019-03-14 18:53:31 -0400

    Added Utils#playerInventoryIsEmpty

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/utils/Utils.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/utils/Utils.java
@@ -1,7 +1,12 @@
 package com.gameservergroup.gsgcore.utils;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.WorldBorder;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Utils {
 
@@ -11,5 +16,10 @@ public class Utils {
         double centerX = worldBorder.getCenter().getX();
         double centerZ = worldBorder.getCenter().getZ();
         return centerX - size > location.getX() || centerX + size <= location.getX() || centerZ - size > location.getZ() || centerZ + size <= location.getZ();
+    }
+
+    public static boolean playerInventoryIsEmpty(Player player) {
+        return Arrays.stream(player.getInventory().getContents()).noneMatch(Objects::nonNull) &&
+                Arrays.stream(player.getInventory().getArmorContents()).noneMatch(itemStack -> itemStack != null && itemStack.getType() != Material.AIR);
     }
 }
