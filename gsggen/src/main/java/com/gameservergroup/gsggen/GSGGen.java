4a9d0f812655337e3f45ec8c2f848fb5b82bbe6b
Trent Williams <proteinshakedev@gmail.com>
2019-03-14 17:53:44 -0400

    Added combattagplus impl

--- gsggen/src/main/java/com/gameservergroup/gsggen/GSGGen.java
+++ gsggen/src/main/java/com/gameservergroup/gsggen/GSGGen.java
@@ -1,14 +1,19 @@
 package com.gameservergroup.gsggen;
 
 import com.gameservergroup.gsgcore.plugin.Module;
+import com.gameservergroup.gsggen.integration.CombatIntegration;
+import com.gameservergroup.gsggen.integration.combat.impl.CombatTagPlusImpl;
 import com.gameservergroup.gsggen.menu.GenMenu;
 import com.gameservergroup.gsggen.units.UnitGen;
+import org.bukkit.plugin.Plugin;
 
 public class GSGGen extends Module {
 
     private static GSGGen instance;
     private UnitGen unitGen;
     private GenMenu genMenu;
+    private boolean enableCombatTagPlusIntegration;
+    private CombatIntegration combatIntegration;
 
     public static GSGGen getInstance() {
         return instance;
@@ -18,6 +23,18 @@ public class GSGGen extends Module {
     public void enable() {
         instance = this;
         saveDefaultConfig();
+        if (getConfig().getBoolean("enable-combattagplus-integration")) {
+            Plugin combatTagPlus = getServer().getPluginManager().getPlugin("CombatTagPlus");
+            if (combatTagPlus != null) {
+                getLogger().info("Enabled CombatTagPlus Integration");
+                this.enableCombatTagPlusIntegration = getConfig().getBoolean("enable-combattagplus-integration");
+                this.combatIntegration = new CombatTagPlusImpl(combatTagPlus);
+            } else {
+                getLogger().severe("You don't have CombatTagPlus installed, if you don't wish to use it disable the integration in GSGGen's config.yml");
+                getPluginLoader().disablePlugin(this);
+                return;
+            }
+        }
         registerUnits(unitGen = new UnitGen());
         this.genMenu = new GenMenu();
         getServer().getScheduler().runTaskTimerAsynchronously(this, () -> getUnitGen().getGenerations().removeIf(next -> !next.generate()), getConfig().getLong("interval"), getConfig().getLong("interval"));
@@ -34,5 +51,13 @@ public class GSGGen extends Module {
 
     public GenMenu getGenMenu() {
         return genMenu;
+    }
+
+    public boolean isEnableCombatTagPlusIntegration() {
+        return enableCombatTagPlusIntegration;
+    }
+
+    public CombatIntegration getCombatIntegration() {
+        return combatIntegration;
     }
 }
