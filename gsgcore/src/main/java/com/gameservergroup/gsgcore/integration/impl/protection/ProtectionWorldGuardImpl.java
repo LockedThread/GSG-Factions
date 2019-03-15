d4ac8396b215d6293688dedcc06fd47e6789905e
Trent Williams <proteinshakedev@gmail.com>
2019-03-15 18:30:06 -0400

    Proper WorldGuard checks for 6.1+

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionWorldGuardImpl.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionWorldGuardImpl.java
@@ -2,19 +2,26 @@ package com.gameservergroup.gsgcore.integration.impl.protection;
 
 import com.gameservergroup.gsgcore.integration.ProtectionIntegration;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.flags.DefaultFlag;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class ProtectionWorldGuardImpl implements ProtectionIntegration {
 
+    private WorldGuardPlugin worldGuardPlugin;
+
+    public ProtectionWorldGuardImpl(Plugin plugin) {
+        this.worldGuardPlugin = (WorldGuardPlugin) plugin;
+    }
+
+
     @Override
     public boolean canBuild(Player player, Location location) {
-        return WorldGuardPlugin.inst().getRegionContainer().createQuery().testState(location, player, DefaultFlag.BLOCK_PLACE);
+        return worldGuardPlugin.canBuild(player, location);
     }
 
     @Override
     public boolean canDestroy(Player player, Location location) {
-        return WorldGuardPlugin.inst().getRegionContainer().createQuery().testState(location, player, DefaultFlag.BLOCK_BREAK);
+        return worldGuardPlugin.canBuild(player, location);
     }
 }
