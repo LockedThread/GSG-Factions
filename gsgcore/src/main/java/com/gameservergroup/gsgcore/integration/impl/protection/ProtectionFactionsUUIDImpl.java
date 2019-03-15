99ed5bc489aa7e470c1f3f4ac01e1896c49e93f0
Trent Williams <proteinshakedev@gmail.com>
2019-03-15 18:29:30 -0400

    Fixed Reflection with FactionsUUID ProtectionIntegration

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionFactionsUUIDImpl.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionFactionsUUIDImpl.java
@@ -1,19 +1,49 @@
 package com.gameservergroup.gsgcore.integration.impl.protection;
 
+import com.gameservergroup.gsgcore.GSGCore;
 import com.gameservergroup.gsgcore.integration.ProtectionIntegration;
-import com.massivecraft.factions.listeners.FactionsBlockListener;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class ProtectionFactionsUUIDImpl implements ProtectionIntegration {
 
     @Override
     public boolean canBuild(Player player, Location location) {
-        return FactionsBlockListener.playerCanBuildDestroyBlock(player, location, "build", true);
+        try {
+            return (boolean) Class.forName("com.massivecraft.factions.listeners.FactionsBlockListener",
+                    false,
+                    GSGCore.getInstance()
+                            .getServer()
+                            .getPluginManager()
+                            .getPlugin("Factions")
+                            .getClass()
+                            .getClassLoader())
+                    .getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, String.class, boolean.class)
+                    .invoke(null, player, location, "build", false);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean canDestroy(Player player, Location location) {
-        return FactionsBlockListener.playerCanBuildDestroyBlock(player, location, "destroy", true);
+        try {
+            return (boolean) Class.forName("com.massivecraft.factions.listeners.FactionsBlockListener",
+                    false,
+                    GSGCore.getInstance()
+                            .getServer()
+                            .getPluginManager()
+                            .getPlugin("Factions")
+                            .getClass()
+                            .getClassLoader())
+                    .getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, String.class, boolean.class)
+                    .invoke(null, player, location, "destroy", false);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
