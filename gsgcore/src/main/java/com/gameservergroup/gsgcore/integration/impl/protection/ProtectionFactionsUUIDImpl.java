b15a8e90eec8981b41f11d11a81fd7a3591d88bd
Trent Williams <proteinshakedev@gmail.com>
2019-03-24 02:22:51 -0400

    Caching fields for increased performance using Reflection

--- gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionFactionsUUIDImpl.java
+++ gsgcore/src/main/java/com/gameservergroup/gsgcore/integration/impl/protection/ProtectionFactionsUUIDImpl.java
@@ -6,13 +6,15 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ProtectionFactionsUUIDImpl implements ProtectionIntegration {
 
-    @Override
-    public boolean canBuild(Player player, Location location) {
+    private Method playerCanBuildDestroyBlock;
+
+    public ProtectionFactionsUUIDImpl() throws ClassNotFoundException {
         try {
-            return (boolean) Class.forName("com.massivecraft.factions.listeners.FactionsBlockListener",
+            this.playerCanBuildDestroyBlock = Class.forName("com.massivecraft.factions.listeners.FactionsBlockListener",
                     false,
                     GSGCore.getInstance()
                             .getServer()
@@ -20,9 +22,17 @@ public class ProtectionFactionsUUIDImpl implements ProtectionIntegration {
                             .getPlugin("Factions")
                             .getClass()
                             .getClassLoader())
-                    .getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, String.class, boolean.class)
-                    .invoke(null, player, location, "build", false);
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+                    .getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, String.class, boolean.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean canBuild(Player player, Location location) {
+        try {
+            return (boolean) playerCanBuildDestroyBlock.invoke(null, player, location, "build", false);
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return false;
@@ -31,17 +41,8 @@ public class ProtectionFactionsUUIDImpl implements ProtectionIntegration {
     @Override
     public boolean canDestroy(Player player, Location location) {
         try {
-            return (boolean) Class.forName("com.massivecraft.factions.listeners.FactionsBlockListener",
-                    false,
-                    GSGCore.getInstance()
-                            .getServer()
-                            .getPluginManager()
-                            .getPlugin("Factions")
-                            .getClass()
-                            .getClassLoader())
-                    .getMethod("playerCanBuildDestroyBlock", Player.class, Location.class, String.class, boolean.class)
-                    .invoke(null, player, location, "destroy", false);
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            return (boolean) playerCanBuildDestroyBlock.invoke(null, player, location, "destroy", false);
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return false;
