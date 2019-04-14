a6d5da7212c2aa11a14a58f9b1d786ba32bd9d13
Admin <lockedthreaddev@gmail.com>
2019-04-14 19:58:59 -0400

    Optimized shop check for check for string because minecraft sucks and has tripwires and string

--- gsgprinter/src/main/java/com/gameservergroup/gsgprinter/integration/impl/selling/ShopGUIPlusSellImpl.java
+++ gsgprinter/src/main/java/com/gameservergroup/gsgprinter/integration/impl/selling/ShopGUIPlusSellImpl.java
@@ -3,11 +3,14 @@ package com.gameservergroup.gsgprinter.integration.impl.selling;
 import com.gameservergroup.gsgprinter.GSGPrinter;
 import com.gameservergroup.gsgprinter.integration.SellIntegration;
 import net.brcdev.shopgui.ShopGuiPlugin;
+import net.brcdev.shopgui.shop.Shop;
+import net.brcdev.shopgui.shop.ShopItem;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ShopGUIPlusSellImpl implements SellIntegration {
@@ -16,13 +19,16 @@ public class ShopGUIPlusSellImpl implements SellIntegration {
 
     public ShopGUIPlusSellImpl() {
         List<String> shops = GSGPrinter.getInstance().getConfig().getStringList("shopguiplus.blacklisted-shops").stream().map(String::toLowerCase).collect(Collectors.toList());
-        ShopGuiPlugin.getInstance().getShopManager()
-                .shops
-                .entrySet()
-                .stream()
-                .filter(entry -> !shops.contains(entry.getKey().toLowerCase()))
-                .flatMap(entry -> entry.getValue().getShopItems().stream())
-                .forEach(shopItem -> prices.putIfAbsent(shopItem.getItem().getType(), shopItem.getBuyPrice()));
+        for (Map.Entry<String, Shop> entry : ShopGuiPlugin.getInstance().getShopManager().shops.entrySet()) {
+            if (!shops.contains(entry.getKey().toLowerCase())) {
+                for (ShopItem shopItem : entry.getValue().getShopItems()) {
+                    if (shopItem.getItem().getType() == Material.STRING) {
+                        prices.putIfAbsent(Material.TRIPWIRE, shopItem.getBuyPrice());
+                    }
+                    prices.putIfAbsent(shopItem.getItem().getType(), shopItem.getBuyPrice());
+                }
+            }
+        }
     }
 
     @Override
