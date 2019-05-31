dba0b2ec879e7750bce93ab94ca266cb38222888
Admin <lockedthreaddev@gmail.com>
2019-05-31 00:58:20 -0400

    build.sh push

---
+++ build.sh
@@ -0,0 +1,18 @@
+# Builds all of the maven projects
+mvn clean install
+
+mv --force --verbose ./factionsplugin/target/Factions-1.6.9.5-U0.2.2.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsgcollectors/target/gsgcollectors.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsgcore/target/gsgcore.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsggen/target/gsggen.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsgprinter/target/gsgprinter.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsgtrenchtools/target/gsgtrenchtools.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+mv --force --verbose ./gsgvouchers/target/gsgvouchers.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
+
+#mv --force --verbose ./gsgoutpost/target/gsgoutpost.jar C:\\Users\\Admin\\Desktop\\GSG-Factions\\plugins
