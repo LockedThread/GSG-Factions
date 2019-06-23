ad6034e9cfa4a3f79c1ab378c21de5041c82515d
LockedThread <proteinshakedev@gmail.com>
2019-06-23 15:52:13 -0400

    Fixed refactor
    Took 17 minutes

--- gsgtrenchtools/src/main/java/com/gameservergroup/gsgcore/integration/coreprotect/CoreProtectIntegration.java
+++
@@ -1,9 +0,0 @@
-package com.gameservergroup.gsgcore.integration.coreprotect;
-
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-
-public interface CoreProtectIntegration {
-
-    void log(Player player, Block block);
-}
